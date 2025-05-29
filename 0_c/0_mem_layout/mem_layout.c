a4add8e8a308ce1787a1e4feae0987f3f92b4cde
VenkateshOdela <venkateshodela14@gmail.com>
2025-05-29 14:37:56 +0530

    Update mem_layout.c
    update

--- 0_c/0_mem_layout/mem_layout.c
+++ 0_c/0_mem_layout/mem_layout.c
@@ -34,7 +34,7 @@ const static int const_stat_uninitvar; // .bss
 const int const_gbl_initvar = 10; // .rodata
 const static int const_stat_initvar = 20; // .rodata
 
-const int const_gbl_uninitvar; // ?? (.bss of a.out but not in .bss of .o) 
+const int const_gbl_uninitvar; // ?? .rodata
 
 
 void func()
@@ -115,4 +115,117 @@ int main() // .text
 /*
 Stack grows in the direction opposite to heap. 
 Stack  :Last in First Out; 
+*/
+
+
+/**
+Disassembly of section .text:
+
+0000000000000000 <func>:
+   0:   f3 0f 1e fa             endbr64 
+   4:   55                      push   %rbp
+   5:   48 89 e5                mov    %rsp,%rbp
+   8:   90                      nop
+   9:   5d                      pop    %rbp
+   a:   c3                      retq   
+
+000000000000000b <gbl_func>:
+   b:   f3 0f 1e fa             endbr64 
+   f:   55                      push   %rbp
+  10:   48 89 e5                mov    %rsp,%rbp
+  13:   48 8d 3d 00 00 00 00    lea    0x0(%rip),%rdi        # 1a <gbl_func+0xf>
+  1a:   e8 00 00 00 00          callq  1f <gbl_func+0x14>
+  1f:   90                      nop
+  20:   5d                      pop    %rbp
+  21:   c3                      retq   
+
+0000000000000022 <stat_func>:
+  22:   f3 0f 1e fa             endbr64 
+  26:   55                      push   %rbp
+  27:   48 89 e5                mov    %rsp,%rbp
+  2a:   48 8d 3d 00 00 00 00    lea    0x0(%rip),%rdi        # 31 <stat_func+0xf>
+  31:   e8 00 00 00 00          callq  36 <stat_func+0x14>
+  36:   90                      nop
+  37:   5d                      pop    %rbp
+  38:   c3                      retq   
+
+0000000000000039 <main>:
+  39:   f3 0f 1e fa             endbr64 
+  3d:   55                      push   %rbp
+  3e:   48 89 e5                mov    %rsp,%rbp
+  41:   48 83 ec 10             sub    $0x10,%rsp
+  45:   c7 45 fc 1e 00 00 00    movl   $0x1e,-0x4(%rbp)
+  4c:   b8 00 00 00 00          mov    $0x0,%eax
+  51:   e8 00 00 00 00          callq  56 <main+0x1d>
+  56:   b8 00 00 00 00          mov    $0x0,%eax
+  5b:   e8 00 00 00 00          callq  60 <main+0x27>
+  60:   b8 00 00 00 00          mov    $0x0,%eax
+  65:   e8 b8 ff ff ff          callq  22 <stat_func>
+  6a:   b8 00 00 00 00          mov    $0x0,%eax
+  6f:   c9                      leaveq 
+  70:   c3                      retq   
+  */
+
+/*
+  Disassembly of section .data:
+
+0000000000000000 <gbl_initvar>:
+   0:   0a 00                   or     (%rax),%al
+        ...
+
+0000000000000004 <stat_initvar>:
+   4:   14 00                   adc    $0x0,%al
+        ...
+
+0000000000000008 <func_stat_initvar.2326>:
+   8:   14 00                   adc    $0x0,%al
+        ... */
+
+/*
+Disassembly of section .bss:
+
+0000000000000000 <stat_uninitvar>:
+   0:   00 00                   add    %al,(%rax)
+        ...
+
+0000000000000004 <func_stat_uninitvar.2327>:
+   4:   00 00                   add    %al,(%rax)
+*/
+
+
+/*
+Disassembly of section .rodata:
+
+0000000000000000 <const_stat_uninitvar>:
+   0:   00 00                   add    %al,(%rax)
+        ...
+
+0000000000000004 <const_gbl_initvar>:
+   4:   0a 00                   or     (%rax),%al
+        ...
+
+0000000000000008 <const_stat_initvar>:
+   8:   14 00                   adc    $0x0,%al
+   a:   00 00                   add    %al,(%rax)
+   c:   67 6c                   insb   (%dx),%es:(%edi)
+   e:   6f                      outsl  %ds:(%rsi),(%dx)
+   f:   62 61 6c 20 66          (bad)
+  14:   75 6e                   jne    84 <main+0x4b>
+  16:   63 74 69 6f             movslq 0x6f(%rcx,%rbp,2),%esi
+  1a:   6e                      outsb  %ds:(%rsi),(%dx)
+  1b:   20 63 61                and    %ah,0x61(%rbx)
+  1e:   6c                      insb   (%dx),%es:(%rdi)
+  1f:   6c                      insb   (%dx),%es:(%rdi)
+  20:   65 64 20 00             gs and %al,%fs:(%rax)
+  24:   73 74                   jae    9a <main+0x61>
+  26:   61                      (bad)  
+  27:   74 69                   je     92 <main+0x59>
+  29:   63 20                   movslq (%rax),%esp
+  2b:   66 75 6e                data16 jne 9c <main+0x63>
+  2e:   63 74 69 6f             movslq 0x6f(%rcx,%rbp,2),%esi
+  32:   6e                      outsb  %ds:(%rsi),(%dx)
+  33:   20 63 61                and    %ah,0x61(%rbx)
+  36:   6c                      insb   (%dx),%es:(%rdi)
+  37:   6c                      insb   (%dx),%es:(%rdi)
+  38:   65 64 20 00             gs and %al,%fs:(%rax)
 */
